bb4a4eb8b50f2a5203d54e8ea7722e8b5ab1d5cf
Tiago <just2009_123@hotmail.com>
2017-07-13 10:37:17 -0300

    teste

---
+++ footer.php
@@ -0,0 +1,10 @@
+	<footer>
+		<div class="container">
+			<a><img class="logo-rodape" src="img/logo-rodape.png" alt="Logo Mirror Fashion"></a>
+			<ul class="social">
+				<li><a href="facebook.com">Facebook</a></li>
+				<li><a href="twitter.com">Twitter</a></li>
+				<li><a href="plus.google.com">Google+</a></li>
+			</ul>
+		</div>
+</footer>
